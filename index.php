0acc01e84332ffe5f42da805c4bd800e86514f6a
plinio <plinio.leal@hotmail.com>
2018-01-27 17:00:14 -0200

    Mais um commit depois de algumas alterações

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+       <h1> Minha index foi alterada! </h1>
         ?>
     </body>
 </html>
